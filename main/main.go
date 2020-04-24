2566b306d9edc53c04665afd07b6cfa6478cfe09
Kevin <seniorpepex@gmail.com>
2020-04-23 23:02:08 -0700

    Add 2nd scrape for sports and 1st scrape for Food

--- main/main.go
+++ main/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	slice := webScraping.NFLWikiScrape()
+	slice := webScraping.NBAWikiScrape()
 	utils.CleanSliceAndWriteToFile("C:/go-work/src/github.com/off-top-go/webscrape-texts", "hel-lo", slice)
 	// EXAMPLES BELOW:
 	// webScraping.MediumScrape()
