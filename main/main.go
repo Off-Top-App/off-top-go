dd2361791872668e7943a904a449b219a63ec016
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2020-06-05 01:55:02 -0700

    including another source of assurance to tell if off-top-go is working on a new developers machine

--- main/main.go
+++ main/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/off-top-go/webScraping"
 	"github.com/off-top-go/webScraping/utils"
 )
 
 func main() {
+	fmt.Println("Running go service!")
 	slice := webScraping.WikiScrape()
 	outputFilePath := "/Users/disjosh/go/src/github.com/off-top-go/webscrape-texts/"
 	utils.CleanSliceAndWriteToFile(outputFilePath, "wiki-sports", slice)
